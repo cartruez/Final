1a2d0a2015d2468ef149f9dd28c5d759550ed1b3
cartruez <84356445+cartruez@users.noreply.github.com>
2021-06-09 11:38:38 -0700

    Initial commit

---
+++ data/COTTONWOOD_7.js
@@ -0,0 +1,8 @@
+var json_COTTONWOOD_7 = {
+"type": "FeatureCollection",
+"name": "COTTONWOOD_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CITY_NO": 0.0, "ZONE": null, "ZONE_CLASS": null, "ZONEGEN_CL": null, "CITY": null, "AREA": 0.0, "URL": "https:\/\/www.koin.com\/news\/protests\/parents-decry-munitions-used-by-feds-near-cottonwood-school\/" }, "geometry": { "type": "Point", "coordinates": [ -122.671686631359364, 45.492540076828327 ] } }
+]
+}
---
+++ data/ICE_6.js
@@ -0,0 +1,8 @@
+var json_ICE_6 = {
+"type": "FeatureCollection",
+"name": "ICE_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CITY_NO": 1.0, "ZONE": "1", "ZONE_CLASS": "1", "ZONEGEN_CL": "1", "CITY": "1", "AREA": 0.0, "URL": "https:\/\/www.opb.org\/article\/2021\/03\/17\/tear-gas-chemical-weapons-portland-ice-cottonwood-school\/" }, "geometry": { "type": "Point", "coordinates": [ -122.672649422549412, 45.492770161230489 ] } }
+]
+}
